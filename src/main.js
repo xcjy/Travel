8a5ec645d03311e6e06fa47e120bae56df42ab84
xcjy <chenjianyuan2@outlook.com>
2019-05-11 15:12:33 +0800

    initial commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import fastClick from 'fastclick'
+
+
+import './assets/style/reset.css' //解决手机默认样式不同的问题
+import './assets/style/border.css' // 解决1像素边框问题
+import './assets/style/iconfont.css'
+
+//300ms延迟问题 fastclick 库
+
+Vue.config.productionTip = false
+fastClick.attach(document.body)
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/home/Home.vue
@@ -0,0 +1,23 @@
+<template>
+<div >
+  <home-header> </home-header>
+ </div>
+ 
+</template> 
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+import HomeHeader from '@/views/home/components/Header.vue'
+export default {
+  name: 'Home',
+  components:{
+    HomeHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
+
