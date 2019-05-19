6b1546902ff40ecc9139f8aecf4ffd645415a6ad
xcjy <chenjianyuan2@outlook.com>
2019-05-19 20:52:59 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
-module.exports= {
-    baseUrl: './',
-    devServer: {
-        proxy: 'http://localhost:8080/'
-    }
-}
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/Travel/'
+      : '/'
+  }
